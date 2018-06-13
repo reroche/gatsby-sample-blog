417c0af73cd3708ee4ab1c0cc4b9d9ad5e435f6d
RE Roche <rers97@gmail.com>
2018-06-13 16:23:40 -0500

    Initial Commit

---
+++ src/pages/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// import Link from 'gatsby-link';
+
+import PostListing from '../components/Posts/PostListing';
+
+const IndexPage = ({ data }) => (
+  <div>
+    <h1>Posts</h1>
+    {data.allMarkdownRemark.edges.map(({ node }) => <PostListing key={node.id} post={node} />)}
+  </div>
+);
+
+IndexPage.propTypes = {
+  data: PropTypes.any,
+};
+
+export default IndexPage;
+
+export const query = graphql`
+  query SiteMeta {
+    site {
+      siteMetadata {
+        title
+        desc
+      }
+    }
+    allMarkdownRemark(sort: {
+      fields: [frontmatter___date],
+      order: DESC
+    }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date
+          }
+          fields {
+            slug
+          }
+          html
+          excerpt
+        }
+      }
+    }
+  }
+`;
